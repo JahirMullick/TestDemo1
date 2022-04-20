1473a8f98969957439bcf4b690169a9f0e690153
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-04-20 16:18:57 +0530

    Fix PUT and DELETE Function now it's working

--- request.js
+++ request.js
@@ -1,16 +1,6 @@
 
 const request = new XMLHttpRequest();
 
-let url
-let id_input 
-let url_id
-
-url  = "http://localhost:3000/student/";
-
-id_input = Number(document.getElementById('giveid').value);
-
-url_id= url.concat(id_input);
-
 function Get_Request() {
     request.open("GET", "http://localhost:3000/student");
     request.send();
@@ -46,33 +36,10 @@ function Post_Request() {
 }
 
 
-
-// function Put_Request() {
-
-//     // let  putData = '{"id": 7,"name": "aool", "phon":09948}';
-//     let url = "http://localhost:3000/student/";
-//     let putData = (document.getElementById('com').value);
-//     let putId = (document.getElementById('giveid').value);
-//     // request.open("PUT", "http://localhost:3000/student/7");
-//     request.open("PUT", url.concat(putId));
-//     request.setRequestHeader('Content-Type', 'application/json')
-//     request.send(putData);
-
-//     request.onload = function () {
-//         if (request.status == 200) {
-//             console.log("Put request send successfully");
-//             document.getElementById('comments1').value = request.response;
-//         }
-//         else {
-//             console.log("Wrong URL!");
-//         }
-//     }
-// }
-
-
 function Put_Request() {
+    let id_input = Number(document.getElementById('giveid').value);
     let putData = (document.getElementById('com').value);
-    request.open("PUT", url_id);
+    request.open("PUT", ("http://localhost:3000/student/" + id_input));
     request.setRequestHeader('Content-Type', 'application/json')
     request.send(putData);
 
@@ -89,30 +56,10 @@ function Put_Request() {
 
 
 
-// function Delete_Request() {
-//     let deleteId = (document.getElementById('com').value);
-//     let url = "http://localhost:3000/student/";
-//     request.open("DELETE", url.concat(deleteId));
-//     request.send();
-
-//     request.onload = function () {
-//         if (request.status == 200) {
-//             console.log("Item Deleted");
-//             document.getElementById('comments1').value = request.response;
-//         }
-//         else {
-//             console.log("Wrong URL!");
-//         }
-//     }
-// }
-
-
-
-
 function Delete_Request() {
-    let deleteId = (document.getElementById('com').value);
-    request.open("DELETE", url_id);
-    request.send(deleteId);
+    let deleteId = (document.getElementById('giveid').value);
+    request.open("DELETE", ("http://localhost:3000/student/" + deleteId));
+    request.send();
 
     request.onload = function () {
         if (request.status == 200) {
